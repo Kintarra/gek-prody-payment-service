86f41bf2a0318f3b8a47cf15f832a45c9a3fabc1
Kintarra <ekaterina.grebennikova@inbox.ru>
2025-07-23 21:58:20 +0300

    TASK-3 Added simple controller (spring-boot)

---
+++ payment-service-app/src/main/java/com/prody/payment/service/app/PaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.prody.payment.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
